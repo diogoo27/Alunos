38a6f12020f096a34dc6bf9e5e2fb53a6206a322
diogoo27 <al22033@espa.edu.pt>
2025-06-17 10:23:16 +0100

    Add files via upload

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AlunosApi.Models.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+}
